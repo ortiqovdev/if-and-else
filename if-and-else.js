3677e9bf5ff371e434b74380bc5f00de028e35c2
ortiqovdev <akmalbekortiqov7976@gmail.com>
2022-12-24 12:16:52 +0500

    if else 1-10 gacha

---
+++ if-and-else.js
@@ -0,0 +1,111 @@
+// * Shart operatoriga oid masalalar
+// * date: 24.12.2022  ,  10:10
+
+// if1
+// let a=-10
+// if(a>0){
+//     console.log(a+1)
+// }else{
+//     console.log(a);
+// }
+
+// if2
+// let a=-10
+// if(a>0){
+//     console.log(a+1)
+// }else{
+//     console.log( a-2 );
+// }
+
+// if3
+// let a=10
+// if(a>0){
+//     console.log(a++);
+// }else if(a==0){
+//     console.log(a+10);
+// }
+// else{
+//     console.log(a-2);
+// }
+
+// if4
+// let a=10
+// let b=20
+// let c=30
+// let i = 0;
+// if (a>0){
+//     console.log(++i);
+// } 
+// if(b > 0) {
+//     console.log(++i);
+// }
+// if(c>0){
+//     console.log(++i);
+// }
+// console.log(i);
+
+
+// if5
+// let a = 2
+// let b = 3
+// let c = -4
+// let i = 0
+// let p=0
+// if (a > 0) {
+//     console.log(++i);
+// }else{
+// console.log(++p);
+// }
+// if (b > 0) {
+//     console.log(++i);
+// }else{
+//     console.log(++p);
+//     }
+// if (c > 0) {
+//     console.log(++i);
+// }else{
+//     console.log(++p);
+//     }
+// console.log(`bunda ${i}ta musbat va ${p}ta manfiy son bor`);
+
+// if6
+//  let a=10
+// let b=20
+// if(a>b){
+//     console.log(`Katta son ${a}`);
+// }else{
+//     console.log(`Katta son ${b}`);
+// }
+
+// if7
+// let a=40
+// let b=20
+// if (a>b){
+//     console.log(`birinchisi katta`);
+// }else{
+//     console.log('ikkinchisi katta');
+// }
+
+// if8
+// let a=120
+// let b=30
+// if (a>b){
+//     console.log(`Kattasi ${a} kichigi ${b}`);
+// }else{
+//     console.log(`Kattasi ${b} kichigi ${a}`); 
+// }
+
+// if9
+// let a = 60
+// let b = 70
+// let c;
+// if (b > a) {
+//     console.log(`${a} ${b}`);
+// } else {
+//     c = b;
+//     b = a;
+//     a = c;
+//     console.log(`${a}  ${b}`);
+// }
+
+// if10
